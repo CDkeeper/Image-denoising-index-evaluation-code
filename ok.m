75599ea7d65dc8458e0736f52beb2a1c5715e957
CDkeeper <92215748+CDkeeper@users.noreply.github.com>
2021-10-18 22:29:02 +0800

    主文件

--- ok.m
+++ ok.m
@@ -1,7 +1,7 @@
 for i = 1:26
-    ref1 = imread(strcat('C:\Users\chenda\Desktop\resources\论文材料\卞通资料与曹佳宁资料\end\or\',num2str(i),'.jpg'));
+    ref1 = imread(strcat('C:\Users\chenda\Desktop\resources\end\or\',num2str(i),'.jpg'));
     ref1 = ref1(:,:,1);
-    ref2 = imread(strcat('C:\Users\chenda\Desktop\resources\论文材料\卞通资料与曹佳宁资料\end\res\1\',num2str(i),'.jpg'));
+    ref2 = imread(strcat('C:\Users\chenda\Desktop\resources\end\res\1\',num2str(i),'.jpg'));
     ref2 = ref2(:,:,1);
     [ssimval, ssimmap] = SSIM(ref1,ref2);
     [peaksnr, snr] = PSNR(ref1,ref2);
@@ -10,9 +10,9 @@ for i = 1:26
     %fprintf('%0.4f\n', RMSE(ref1,ref2));
 end
 
-%     ref1 = imread(strcat('C:\Users\chenda\Desktop\GI图\Lincoln.png'));
+%     ref1 = imread(strcat('C:\Users\chenda\Desktop\GI鍥綷Lincoln.png'));
 %     ref1 = ref1(:,:,1);
-%     ref2 = imread(strcat('C:\Users\chenda\Desktop\GI图\Lincoln_GI_SwinIR_large.png'));
+%     ref2 = imread(strcat('C:\Users\chenda\Desktop\GI鍥綷Lincoln_GI_SwinIR_large.png'));
 %     ref2 = ref2(:,:,1);
 %     [ssimval, ssimmap] = SSIM(ref1,ref2);
 %     [peaksnr, snr] = PSNR(ref1,ref2);
